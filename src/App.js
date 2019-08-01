e8df7c60ec169dc3fc64af161678b413d7b871cf
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-08-01 11:10:59 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
@@ -10,12 +11,13 @@ import Error from "./pages/Error";
 import './App.css';
 
 function App() {
-  return <>
-  <Home />
-  <Rooms />
-  <SingleRoom />
-  <Error />
-  </>;
+  return (
+    <>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms/" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={SingleRoom} /> 
+    </> 
+    );
 }
 
 export default App;
