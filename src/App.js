2401de514a4aad7f9b0ebb4ee0ce0a4b4411b952
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-08-01 12:46:08 +0100

    Navbar done

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <Route component={Navbar} />
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
