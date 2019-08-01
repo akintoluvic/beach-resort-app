a75b65f072b7af66ec1c122553a101416d978644
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-08-01 11:20:13 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
@@ -13,6 +14,7 @@ import './App.css';
 function App() {
   return (
     <>
+      <Route component={Navbar} />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
