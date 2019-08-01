e6cdf4f963702e668cd8c2e69267df79181fd393
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-08-01 15:01:49 +0100

    services setup

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
 import Title from './Title';
 
 export default class Services extends Component {
+    state={
+        services:[
+            {
+                icon:<FaCocktail />, 
+                title: "Free Cocktails",
+                info: "Lorem jdjd dkdd djis ejir ehwjw jrjr wjwje, dkdd djis ejir ehwjw jrjr wjwje ejej!"
+            },{
+                icon:<FaHiking />, 
+                title: "Free Hiking",
+                info: "Lorem jdjd dkdd djis ejir ehwjw jrjr wjwje, dkdd djis ejir ehwjw jrjr wjwje ejej!"
+            },{
+                icon:<FaShuttleVan />, 
+                title: "free shuttle",
+                info: "Lorem jdjd dkdd djis ejir ehwjw jrjr wjwje, dkdd djis ejir ehwjw jrjr wjwje ejej!"
+            },{
+                icon:<FaBeer />, 
+                title: "free beer",
+                info: "Lorem jdjd dkdd djis ejir ehwjw jrjr wjwje, dkdd djis ejir ehwjw jrjr wjwje ejej!"
+            }
+        ]
+    }
     render() {
         return (
-            <div>
+            <section className="services">
                 <Title title="services" />
-            </div>
+                <div className="services-center">
+                    {this.state.services.map((item,index) => {
+                        return (
+                        <article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                        );
+                    })}
+                </div>
+            </section>
         );
     }
 }
