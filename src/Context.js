632948bbdca67b7541e5246870cee4e2f63db45f
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-08-03 15:37:19 +0100

    passed data to context

--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import items from "./data"
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
     state = {
-        greeting: "hello",
-        name: "John"
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
+    };
+
+    // getData
+
+    componentDidMount(){
+        let rooms = this.formatData(items);
+        console.log(rooms);
+    }
+
+    formatData(items){
+        let tempItems = items.map(item => {
+            let id = item.sys.id;
+            let images = item.fields.images.map(image => image.fields.file.url);
+            let room = {...item.fields, images, id};
+            return room;
+        });
+        return tempItems;
     }
     render() {
         return (
--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -5,10 +5,9 @@ import { RoomContext } from "../Context";
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
     render() {
-        const {name,greeting} = this.context;
         return (
             <div>
-                {greeting} {name} from FeaturedRooms
+                 from FeaturedRooms
             </div>
         )
     }
