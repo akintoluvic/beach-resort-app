4636015bf43782d264bc48e4a24eac2d16e38c1b
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-08-03 16:36:20 +0100

    featured rooms displays correctly

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -8,13 +8,17 @@ import Loading from "./Loading";
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
     render() {
-        const { featuredRooms: rooms } = this.context;
-        console.log(rooms);
+        let { loading, featuredRooms: rooms } = this.context;
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
         return (
-            <div>
-                 from FeaturedRooms
-                 <Loading />
-            </div>
+            <section className="featured-rooms" >
+                 <Title title="featured rooms" />
+                 <div className="featured-rooms-center">
+                     {loading?<Loading />:rooms}
+                 </div>
+            </section>
         )
     }
 }
