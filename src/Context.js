2656e6e231ee289447c60ddfc224d49c6ee78d8e
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-08-03 15:52:27 +0100

    got data into featured rooms component

--- src/Context.js
+++ src/Context.js
@@ -15,7 +15,10 @@ class RoomProvider extends Component {
 
     componentDidMount(){
         let rooms = this.formatData(items);
-        console.log(rooms);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        this.setState({
+            rooms, featuredRooms, sortedRoom: rooms, loading: false
+        });
     }
 
     formatData(items){
