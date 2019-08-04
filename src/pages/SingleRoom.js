4e009519d85847aa64bc16a27cba6643c89afcfc
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-08-04 15:58:07 +0100

    catch error if room is undefined

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from 'react-router-dom'
+import { RoomContext } from "../Context";
 
 class SingleRoom extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        // console.log(this.props);
+        this.state={
+            slug:this.props.match.params.slug,
+            defaultBcg,
+        };
     }
+    static contextType = RoomContext;
+    // componentDidMount() {}
     render() { 
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug)
+        console.log(room);
+        if(!room) {
+            return <div className="error">
+                <h3>no such room could be found...</h3>
+                <Link to="/rooms" className="btn-primary">back to rooms</Link>
+            </div>
+        }
         return ( 
-            <div>Hello from Single Room</div>
+            <div>Hello from Single Room {room.name}</div>
          );
     }
 }
