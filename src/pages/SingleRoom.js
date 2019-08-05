604d9599bedd9a1e7cdc31a5d15a7a9592e52ebe
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-08-05 13:07:52 +0100

    rendering room images

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -20,7 +20,6 @@ class SingleRoom extends Component {
     render() { 
         const {getRoom} = this.context;
         const room = getRoom(this.state.slug)
-        console.log(room);
         if(!room) {
             return <div className="error">
                 <h3>no such room could be found...</h3>
@@ -28,12 +27,22 @@ class SingleRoom extends Component {
             </div>
         }
         const { name, description, capacity, size,price,extras,breakfast,pets,images} = room;
+        const [mainImg,...defaultImg] = images;
         return ( 
-            <StyledHero img={images[0]}>
+            <>
+            <StyledHero img={images[0] || this.state.defaultBcg}>
                 <Banner title={`${name} room`}>
                     <Link to="/rooms" className="btn-primary">back to rooms</Link>
                 </Banner>
             </StyledHero>
+            <section className="single-room">
+                <div className="single-room-images">
+                    {defaultImg.map((item,index) => 
+                        <img key={index} src={item} alt={name} />
+                    )}
+                </div>
+            </section>
+            </>
          );
     }
 }
