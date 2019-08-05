5ee80df9fa9f7631d97ce2d5aad1c86101f824d9
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-08-05 13:21:19 +0100

    single rooms completed

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -41,6 +41,33 @@ class SingleRoom extends Component {
                         <img key={index} src={item} alt={name} />
                     )}
                 </div>
+            
+                <div className="single-room-info">
+                    <article className="desc">
+                        <h3>details</h3>
+                        <p>{description}</p>
+                    </article>
+                    <article className="info">
+                        <h3>info</h3>
+                        <h6>price: ${price}</h6>
+                        <h6>size: ${size} SQFT</h6>
+                        <h6>
+                            max capacity: {
+                                capacity > 1 ? `${capacity} people` : `${capacity} person` 
+                            }
+                        </h6>
+                        <h6>{pets?"pets":"no pets allowed"}</h6>
+                        <h6>{breakfast && "free breakfast included"}</h6>
+                    </article>
+                </div>
+            </section>
+            <section className="room-extras">
+                <h6>extras</h6>
+                <ul className="extras">
+                    {extras.map((item,index) =>{
+                        return <li key={index}>- {item}</li>
+                    })}
+                </ul>
             </section>
             </>
          );
