c3d57ab0c4be1f8ba6e55322f940f68b702b2b65
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-08-07 14:55:53 +0100

    room type filter done

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -3,14 +3,22 @@ import {useContext} from 'react'
 import {RoomContext} from '../Context'
 import Title from '../components/Title'
 
+// get all unique values
+const getUnique = (items,value) => {
+    return [...new Set(items.map(item => item[value]))]
+}
 
 export default function RoomsFilter() {
     const context = useContext(RoomContext);
     const {
-        handleChange, type, capacity, 
+        rooms, handleChange, type, capacity, 
         price, minPrice, maxPRice, 
         minSize, maxSize, pets
     } = context;
+    // get unique types
+    let types = getUnique(rooms, 'type');
+    // add all
+    types = ['all', ...types]
     return (
         <section className="filter-container" >
             <Title title="search rooms" />
@@ -19,7 +27,12 @@ export default function RoomsFilter() {
                 <div className="form-group">
                     <label htmlFor="type">room type</label>
                     <select name="type" id="type" value={type} 
-                    className="form-control" onChange={handleChange} ></select>
+                    className="form-control" onChange={handleChange}
+                     >
+                         {types.map((item, index) =>
+                             <option value={item} key={index}>{item}</option>
+                         )}
+                     </select>
                 </div>
                 {/* end select type */}
             </form>
