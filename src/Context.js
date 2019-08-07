c6c61bbe2448b8bd3f26d19833faa7846488b6bf
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-08-07 14:36:45 +0100

    init

--- src/Context.js
+++ src/Context.js
@@ -8,16 +8,35 @@ class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: true,
+        type:'all',
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
     };
 
     // getData
 
     componentDidMount(){
         let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
+        let featuredRooms = rooms.filter(room => 
+            room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item =>
+            item.price));
+        let maxSize = Math.max(...rooms.map(item =>
+            item.size));
         this.setState({
-            rooms, featuredRooms, sortedRooms: rooms, loading: false
+            rooms, 
+            featuredRooms, 
+            sortedRooms: rooms, 
+            loading: false, 
+            maxPrice,
+            maxSize
         });
     }
 
@@ -37,9 +56,23 @@ class RoomProvider extends Component {
         return room;
     }
 
+    handleChange = (e) => {
+        const type = e.target.type
+        const name = e.target.name
+        const value = e.target.value
+        console.log(type,name,value)
+    }
+
+    filterRooms = () => {
+        console.log("hello");
+    }
+
     render() {
         return (
-            <RoomContext.Provider value={{...this.state, getRoom:this.getRoom}} >
+            <RoomContext.Provider value={{...this.state, 
+                getRoom:this.getRoom, 
+                handleChange: this.handleChange
+                }} >
                 {this.props.children}
             </RoomContext.Provider>
         )
--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {useContext} from 'react'
+import {RoomContext} from '../Context'
+import Title from '../components/Title'
+
 
 export default function RoomsFilter() {
+    const context = useContext(RoomContext);
+    const {
+        handleChange, type, capacity, 
+        price, minPrice, maxPRice, 
+        minSize, maxSize, pets
+    } = context;
     return (
-        <div>
-            Hello from Rooms Filter
-        </div>
-    )
+        <section className="filter-container" >
+            <Title title="search rooms" />
+            <form className="filter-form">
+                {/* select type */}
+                <div className="form-group">
+                    <label htmlFor="type">room type</label>
+                    <select name="type" id="type" value={type} 
+                    className="form-control" onChange={handleChange} ></select>
+                </div>
+                {/* end select type */}
+            </form>
+        </section>
+    );
 }
