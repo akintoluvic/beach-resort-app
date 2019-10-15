dfa269b47a0aac9e5e3c451c95901cca20ce242c
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-15 18:09:52 +0100

    added env file

--- src/Context.js
+++ src/Context.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import items from "./data"
+// import items from "./data"
+import Client from "./Contentful"
+
 
 const RoomContext = React.createContext();
 
@@ -22,22 +24,33 @@ class RoomProvider extends Component {
 
     // getData
 
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: "lagosBeachResortRooms"
+            });
+            let rooms = this.formatData(response.items);
+            let featuredRooms = rooms.filter(room => 
+                room.featured === true);
+            let maxPrice = Math.max(...rooms.map(item =>
+                item.price));
+            let maxSize = Math.max(...rooms.map(item =>
+                item.size));
+            this.setState({
+                rooms, 
+                featuredRooms, 
+                sortedRooms: rooms, 
+                loading: false, 
+                maxPrice,
+                maxSize
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     componentDidMount(){
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => 
-            room.featured === true);
-        let maxPrice = Math.max(...rooms.map(item =>
-            item.price));
-        let maxSize = Math.max(...rooms.map(item =>
-            item.size));
-        this.setState({
-            rooms, 
-            featuredRooms, 
-            sortedRooms: rooms, 
-            loading: false, 
-            maxPrice,
-            maxSize
-        });
+        this.getData()
     }
 
     formatData(items){
